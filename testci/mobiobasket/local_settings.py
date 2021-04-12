bf5b4796c4989ea00b6ab630a6542f1055206ddd
kevalmobio <kevals.mobio@gmail.com>
2021-04-12 12:38:27 +0530

    testing

---
+++ testci/mobiobasket/local_settings.py
@@ -0,0 +1,15 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mobiobasketdb',
+        'USER': 'keval',
+        'PASSWORD': '123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+REQUEST_SCHEME = "https"
+
+EMAIL_HOST_USER = 'kevals.mobio@gmail.com'
+EMAIL_HOST_PASSWORD = 'Mobio@2599'
---
+++ testci/mobiobasket/local_settings_template.py
@@ -0,0 +1,16 @@
+import os
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABSE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
+    }
+}
+
+REQUEST_SCHEME = "https"
+
+EMAIL_HOST_USER = 'admin@gmail.com'
+EMAIL_HOST_PASSWORD = 'admin@123'
